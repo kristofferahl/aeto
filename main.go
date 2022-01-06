390175c1bf5bfefd366be29fd77d24a2ea2629d3
Kristoffer Ahl <mail@77dynamite.com>
2022-01-06 21:41:25 +0100

    Scaffoling of ResourceSet.

--- main.go
+++ main.go
@@ -99,6 +99,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Blueprint")
 		os.Exit(1)
 	}
+	if err = (&controllers.ResourceSetReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ResourceSet")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
