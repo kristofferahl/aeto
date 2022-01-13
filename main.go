d65657bd79db82add46a84e05ba45a37c3b85afe
Kristoffer Ahl <mail@77dynamite.com>
2022-01-13 11:33:32 +0100

    Scaffolding of CertificateConnector.

--- main.go
+++ main.go
@@ -188,6 +188,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Certificate")
 		os.Exit(1)
 	}
+	if err = (&acmawscontrollers.CertificateConnectorReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CertificateConnector")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
